96e975e218760b73127c5cf044e6e2e20c4e688c
samarth nagar <samarth07nagar@gmail.com>
2025-03-27 20:33:45 +0530

    main backup

---
+++ env_confs/user.js
@@ -0,0 +1,7 @@
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("browser.tabs.drawInTitlebar", true);
+user_pref("browser.uidensity", 0);
+user_pref("layers.acceleration.force-enabled", true);
+user_pref("mozilla.widget.use-argb-visuals", true);
+user_pref("widget.gtk.rounded-bottom-corners.enabled", true);
+user_pref("svg.context-properties.content.enabled", true);
